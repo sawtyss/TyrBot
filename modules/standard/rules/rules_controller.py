88d0a2551d6465b1e0766cb2e24d49b1ffb737b4
Greg Gunia <sawtyss@gmail.com>
2020-06-24 16:54:59 -0400

    Support adding rules

--- modules/standard/rules/rules_controller.py
+++ modules/standard/rules/rules_controller.py
@@ -1,6 +1,6 @@
-from core.command_param_types import Any
+from core.command_param_types import Any, Const
 from core.decorators import instance, command
-
+from core.chat_blob import ChatBlob
 
 @instance()
 class RulesController:
@@ -12,7 +12,7 @@ class RulesController:
         self.db: DB = registry.get_instance("db")
 
     def start(self):
-        self.db.exec("CREATE TABLE IF NOT EXISTS rules (id INT PRIMARY KEY AUTO_INCREMENT, rule TEXT")
+        self.db.exec("CREATE TABLE IF NOT EXISTS rules (id INT PRIMARY KEY AUTO_INCREMENT, rule TEXT)")
 
     @command(command="rules",params=[], access_level="member", description="Show rules")
     def rules_cmd(self, request):
@@ -22,10 +22,14 @@ class RulesController:
         else:
             return ChatBlob("Rules", "No rules!\n\n LONG LIVE ANARCHY!")
 
-    @command(command="rules",params=[Const("add"),Any("rule")], access_level="admin", description="Add a new rule")
-    def rules_add_cmd(self, request, rule):
-
-        return "New rule number {} has been added."
+    @command(command="rules_admin",params=[Const("add"),Any("rule")], access_level="admin", description="Add a new rule")
+    def rules_admin_add_cmd(self, request, _, rule):
+        sql = "INSERT INTO rules (rule) VALUES (?)"
+        success = self.db.exec(sql, [rule])
+        if success > 0:
+            return "New rule with number <highlight>%d<end> added successfully." % self.db.last_insert_id()
+        else:
+            return "Failed to add news entry."
 
     def format_rules_entries(self, entries):
         blob = ""
